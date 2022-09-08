bc328aed3e5e26b26e0a016d96de0c02aa6e3537
vicky1236 <vickygowdabg@gmail.com>
2022-09-08 11:17:04 +0530

    added trivy shell script

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
-echo $dockerImage=Name
+dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile) ##--severity value 
+echo $dockerImageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --serverity HIGH --light $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --serverity CRITICAL --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light $dockerImageName
 
      # Trivy scan result processing
      exit_code=$?
